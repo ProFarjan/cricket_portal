d32ae8516bd8720857bfa29ccb90ebf7d42afc58
zaheenrakib <rakibislambest@gmail.com>
2024-12-12 11:42:20 +0600

    some change

--- src/components/post/post-format/PostFormatMatch.jsx
+++ src/components/post/post-format/PostFormatMatch.jsx
@@ -12,7 +12,6 @@ import SocialShareSide from "./elements/SocialShareSide";
 import Image from "next/image";
 
 const PostFormatMatch = ({ postData }) => {
-
   return (
     <>
       {/* <MetaDataOne metaData={postData} /> */}
@@ -30,31 +29,63 @@ const PostFormatMatch = ({ postData }) => {
                           {/*-------Left-----Side------*/}
                           <div className="col-span-1 border-2 border-orange-600">
                             <p>
-                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-                              ipsum asperiores voluptate exercitationem similique quidem ad labore
-                              sed voluptates id omnis. Accusamus ad cum exercitationem sint minus,
-                              ipsum voluptatem quo, culpa quibusdam modi vel nisi similique ea.
-                              Minus sed libero dicta saepe assumenda illo exercitationem
-                              aspernatur sint consequuntur quaerat commodi officiis, provident nam
-                              recusandae inventore et accusantium cum. Natus repellat nobis,
-                              accusantium odit officia illum aperiam labore atque aliquid vel.
-                              Blanditiis, repudiandae voluptatem. Error, pariatur aut iure in
-                              placeat neque.
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Iste ipsum asperiores voluptate
+                              exercitationem similique quidem ad labore sed
+                              voluptates id omnis. Accusamus ad cum
+                              exercitationem sint minus, ipsum voluptatem quo,
+                              culpa quibusdam modi vel nisi similique ea. Minus
+                              sed libero dicta saepe assumenda illo
+                              exercitationem aspernatur sint consequuntur
+                              quaerat commodi officiis, provident nam recusandae
+                              inventore et accusantium cum. Natus repellat
+                              nobis, accusantium odit officia illum aperiam
+                              labore atque aliquid vel. Blanditiis, repudiandae
+                              voluptatem. Error, pariatur aut iure in placeat
+                              neque.
                             </p>
                           </div>
                           {/*-------Right-----Side------*/}
                           <div className="md:col-span-3 border-2 border-cyan-600">
                             <h1 className="text-blue-600 text-5xl">News</h1>
-                            <h1>Rohit: 'We were not good enough with bat'</h1>
-                            <p>He also defends picking Harshit Rana for the Adelaide game: "Rana didn't do anything wrong in the first Test"</p>
-                            <img className="w-full"
-                              src={process.env.NEXT_PUBLIC_IMGPATH + postData[0]?.image} />
+                            <h3>{postData[0]?.title}</h3>
+                            <div>
+                              <div className="flex justify-between">
+                                <div className="flex ">
+                                  <img
+                                    className="rounded-full"
+                                    src="https://img1.hscicdn.com/image/upload/f_auto,t_h_100/lsci/db/PICTURES/CMS/73900/73941.1.jpg"
+                                    alt=""
+                                  />
+                                  <div className="flex flex-col justify-center items-start">
+                                    <h1 className="text-lg m-0">Andrew McGlashan</h1>
+                                    <h1 className="text-xs">
+                                      09-Dec-2024 <span>1 hr ago</span>
+                                    </h1>
+                                  </div>
+                                </div>
+                                <div>
+                                  <span>hello</span>
+                                  <span>hello</span>
+                                </div>
+                              </div>
+                            </div>
+                            <img
+                              className="w-full"
+                              src={
+                                process.env.NEXT_PUBLIC_IMGPATH +
+                                postData[0]?.image
+                              }
+                            />
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: postData[0]?.detail,
+                              }}
+                            ></div>
                           </div>
                         </div>
                       </div>
                     </div>
-
-
                     {/* <div className="flex">
                       <div className="w-96 bg-red-600">
                         <h1>Hello</h1>
@@ -88,6 +119,6 @@ const PostFormatMatch = ({ postData }) => {
       </div>
     </>
   );
-}
+};
 
 export default PostFormatMatch;
