066c8f108bb8f422e42af476ad5d838b9f3847e4
AkiburNeowaz <akiburneowaz@gmail.com>
2024-12-13 00:15:38 +0600

    ranking page update

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -81,59 +81,85 @@ const data = {
     },
 };
 
-// Sorting data to ensure Pos 1 is at the top
 const sortData = (data) => {
     return [...data].sort((a, b) => a.position - b.position);
 };
 
 const RankingsCard = ({ title, data, imageUrl }) => {
-    const sortedData = sortData(data); // Sort the data by position
+    const sortedData = sortData(data);
     return (
-        <div className="bg-white border m-2 flex flex-col">
+        <div className="bg-white border m-2  flex flex-col">
             <div
                 className="relative h-20"
                 style={{ backgroundImage: `url(${imageUrl})` }}
             >
-                <div className="absolute inset-0 bg-blue-950"></div>
-                <h3 className="absolute py-6 px-14 text-xl text-white">{title}</h3>
+                <div className="absolute inset-0 bg-blue-950 ">
+                    <h3 className="absolute py-6 px-14 text-xl font-medium text-white">
+                    {title}
+                </h3>
+                </div>
+                
             </div>
             <div className="p-2 text-lg">
-                <table className="w-full">
-                    <thead>
-                        <tr className="border-b-2 border-gray-200">
-                            <th className="px-2 py-2 text-center">Pos</th>
-                            <th className="px-2 py-2">Team/Player</th>
-                            <th className="px-2 py-2 text-right">Rating</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {sortedData.map((item) => (
-                            <tr
-                                className={
+                <div className="flex flex-col">
+                    {/* Header Row */}
+                    <div className="flex items-center justify-between  py-2 ">
+                        <span className="text-center font-bold text-gray-600 w-1/6">Pos</span>
+                        <span className="text-left font-bold text-gray-600 w-2/3">Team</span>
+                        <span className="text-right font-bold text-gray-600 w-1/6">Rating</span>
+                    </div>
+
+                    {sortedData.map((item) => (
+                        <div
+                            key={item.position}
+                            className={
+                                `flex items-start justify-between py-3 border-t-2 border-gray-100   ${
                                     item.position === 1
-                                        ? 'text-black font-extrabold text-3xl py-4 border-b-2 border-gray-200'
-                                        : 'bg-white text-gray-800 border-t-2'
+                                        ? '  h-32 bg-gray-100 items-center font-extrabold text-black'
+                                        : 'bg-white text-gray-800'
+                                }`
+                            }
+                        >
+                            <span
+                                className={
+                                    `text-center font-semibold w-1/6 ${
+                                        item.position === 1
+                                            ? 'text-4xl'
+                                            : 'text-lg font-semibold border-r-2 border-gray-200'
+                                    }`
                                 }
-                                key={item.position}
                             >
-                                <td className="p-2 py-3 text-center font-semibold">{item.position}</td>
-                                <td
+                                {item.position}
+                            </span>
+                            <div className="flex items-center w-2/3">
+                                {item.flag && (
+                                    <img
+                                        src={item.flag}
+                                        alt="flag"
+                                        className="w-10 h-8 mr-2"
+                                    />
+                                )}
+                                <span
                                     className={
                                         item.position === 1
-                                            ? 'px-2 py-2 flex my-8 text-2xl font-medium'
-                                            : 'px-2 flex py-3 text-xl font-medium'
+                                            ? 'text-4xl font-medium'
+                                            : 'text-xl font-semibold '
                                     }
                                 >
-                                    {item.flag && (
-                                        <img src={item.flag} alt="flag" className="w-8 h-6 mr-2" />
-                                    )}
                                     {item.team || item.player}
-                                </td>
-                                <td className="px-2 py-3 text-right font-semibold">{item.rating}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                                </span>
+                            </div>
+                            <span  className={
+                                        item.position === 1
+                                            ? 'text-4xl font-medium '
+                                            : 'text-xl font-semibold '
+                                    }
+                                    >
+                                {item.rating}
+                            </span>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="text-center mt-auto">
                 <button className="bg-gray-100 w-full p-2 hover:text-white hover:bg-blue-950 font-semibold md:text-2xl duration-300">
@@ -147,29 +173,32 @@ const RankingsCard = ({ title, data, imageUrl }) => {
 
 const ICCRankings = () => {
     return (
-        <div className="p-3">
+        <div className="p-4 mt-4">
             <div className="bg-white-100 min-h-screen">
-                <div className="container mx-auto p-8 bg-white shadow-md">
-                    <h1 className="text-3xl  font-bold ml-4 ">ICC Men Team Rankings</h1>
-                    <div className="grid h-[300px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <RankingsCard  title="Test-Team Rankings" data={data?.test} imageUrl="https://example.com/test-team-image.jpg" />
+                <div className="container mx-auto p-8 bg-white shadow-md rounded-2xl">
+                    <h1 className="text-3xl font-bold ml-4">ICC Men Team Rankings</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        <RankingsCard title="Test-Team Rankings" data={data?.test} imageUrl="https://example.com/test-team-image.jpg" />
                         <RankingsCard title="ODI-Team Rankings" data={data?.odi} imageUrl="https://example.com/odi-team-image.jpg" />
                         <RankingsCard title="T20I-Team Rankings" data={data?.t20i} imageUrl="https://example.com/t20i-team-image.jpg" />
                     </div>
-                    <h1 className="text-3xl font-bold my-40 ml-4 mb-4">ICC Men Batting Rankings</h1>
-                    <div className="h-[300px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                    <h1 className="text-3xl font-bold my-24 ml-4 mb-4">ICC Men Batting Rankings</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <RankingsCard title="Test-Batting Rankings" data={data?.batting?.test} imageUrl="https://example.com/test-batting-image.jpg" />
                         <RankingsCard title="ODI-Batting Rankings" data={data?.batting?.odi} imageUrl="https://example.com/odi-batting-image.jpg" />
                         <RankingsCard title="T20I-Batting Rankings" data={data?.batting?.t20i} imageUrl="https://example.com/t20i-batting-image.jpg" />
                     </div>
+
                     <h1 className="text-3xl font-bold my-20 ml-4 mb-4">ICC Men Bowling Rankings</h1>
-                    <div className="h-[300px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <RankingsCard title="Test-Bowling Rankings" data={data?.bowling?.test} imageUrl="https://example.com/test-bowling-image.jpg" />
                         <RankingsCard title="ODI-Bowling Rankings" data={data?.bowling?.odi} imageUrl="https://example.com/odi-bowling-image.jpg" />
                         <RankingsCard title="T20I-Bowling Rankings" data={data?.bowling?.t20i} imageUrl="https://example.com/t20i-bowling-image.jpg" />
                     </div>
+
                     <h1 className="text-3xl font-bold my-20 ml-4 mb-4">ICC Men Allrounder Rankings</h1>
-                    <div className="h-[300px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <RankingsCard title="Test-Allrounder Rankings" data={data?.allrounder?.test} imageUrl="https://example.com/test-allrounder-image.jpg" />
                         <RankingsCard title="ODI-Allrounder Rankings" data={data?.allrounder?.odi} imageUrl="https://example.com/odi-allrounder-image.jpg" />
                         <RankingsCard title="T20I-Allrounder Rankings" data={data?.allrounder?.t20i} imageUrl="https://example.com/t20i-allrounder-image.jpg" />
